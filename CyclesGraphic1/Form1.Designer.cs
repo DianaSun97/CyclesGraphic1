8678a2dcbf6e963b11732d28735462173b2be493
Diana <72194867+DianaSun97@users.noreply.github.com>
2022-05-26 22:39:19 +0300

    Initial commit

---
+++ CyclesGraphic1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CyclesGraphic1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.butt = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.picture.BackColor = System.Drawing.Color.White;
+            this.picture.Location = new System.Drawing.Point(5, 5);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(600, 600);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            this.picture.Click += new System.EventHandler(this.picture_Click);
+            // 
+            // butt
+            // 
+            this.butt.Location = new System.Drawing.Point(627, 12);
+            this.butt.Name = "butt";
+            this.butt.Size = new System.Drawing.Size(161, 50);
+            this.butt.TabIndex = 1;
+            this.butt.Text = "Create(1)";
+            this.butt.UseVisualStyleBackColor = true;
+            this.butt.Click += new System.EventHandler(this.butt_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(627, 200);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(161, 54);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(627, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Create(2)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(629, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 57);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Create(3)\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 661);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.butt);
+            this.Controls.Add(this.picture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize) (this.picture)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Button Clear;
+
+        private System.Windows.Forms.Button butt;
+
+        private System.Windows.Forms.PictureBox picture;
+
+        #endregion
+    }
+}
---
+++ CyclesGraphic1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CyclesGraphic1
+{
+    public partial class Form1 : Form
+    {
+        //Инструменты для рисования
+        public Graphics g;
+        public Pen pen;
+        
+        //Конструктор
+        public Form1()
+        {
+            InitializeComponent();
+            InitGraph();
+        }
+
+        void InitGraph()
+        {
+            Bitmap bmp = new Bitmap(picture.Width, picture.Height);
+            //Отображение на экране
+            picture.Image = bmp;
+            g = Graphics.FromImage(picture.Image);
+            pen = new Pen(Color.Red);
+            g.DrawLine(pen,0,0,600,600);
+            
+        }
+
+      
+
+        //привязка к кнопке
+        private void picture_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void butt_Click(object sender, EventArgs e)
+        {
+            
+            //написание координат с помощью цикла
+            for (int y = 0; y <=600; y+=10)
+            {
+                g.DrawLine(pen,0,y,600,y);
+                picture.Refresh();
+            }
+            
+             //ручное написание координат 
+            // g.DrawLine(pen,0,0,600,0);
+            // g.DrawLine(pen,0,10,600,10);
+            // g.DrawLine(pen,0,20,600,20);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            //очистка после рефреша
+            g.Clear(Color.White);
+            picture.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x<=600; x+=10)
+            {
+                pen = new Pen(Color.DarkOrchid);
+                g.DrawLine(pen,x,600,x,0);
+                picture.Refresh();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pen = new Pen(Color.OrangeRed);
+            
+            for (int a = 0; a<=600; a+=10)
+            {
+                g.DrawLine(pen,a,0,0,a);
+                picture.Refresh();
+            }
+            
+            pen = new Pen(Color.Chartreuse);
+            
+            for (int a = 0; a<=600; a+=10)
+            {
+                g.DrawLine(pen,a,600,600,a);
+                picture.Refresh();
+            }
+            
+           
+        }
+    }
+}
